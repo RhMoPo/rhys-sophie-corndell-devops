eb52dce58415ba8cecd6c6130b3bf1b0bd8dfa85
Foster Sophie (ETAS-DAP/XPC-UK) <fos1yok@bosch.com>
2024-11-12 14:27:23 +0000

    step 2

--- afternoon-task.py
+++ afternoon-task.py
@@ -29,4 +29,28 @@ def calculator():
 
         print("result", result)
 
-calculator()
+def calculator2(operation, A, B):
+    # we want to prompt for the users input
+    # carry out calc based on user input 
+    if operation == "+":
+        result = A + B
+    elif operation == "-":
+        result = A - B
+    elif operation == "x":
+        result = A * B
+    elif operation == "/":
+        result = 0 if B == 0 else A / B
+    # output the result 
+
+
+    return result
+#calculator()
+
+#open txt file
+f = open('step_2.txt')
+lines = f.read().splitlines()
+total = 0
+for i in lines:
+    line = i.split()
+    total += calculator2(line[1], int(line[2]), int(line[3]))
+print(total)
